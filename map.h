3c972fa84a79a2b089ed9c84c6e58166e3c1c481
l1eju <wengjulie@hotmail.com>
2024-11-26 15:28:27 +0100

    Retouche finale du menu

--- map.h
+++ map.h
@@ -5,6 +5,8 @@
 #ifndef UNTITLED1_MAP_H
 #define UNTITLED1_MAP_H
 
+#include "loc.h"
+
 #define COST_UNDEF 65535
 /**
  * @brief Enum for the possible soils of the map
@@ -33,6 +35,9 @@ typedef struct s_map
     int     x_max;
     int     y_max;
 } t_map;
+
+t_position getBaseStationPosition(t_map);
+
 
 /**
  * @brief Function to initialise the map from a file
